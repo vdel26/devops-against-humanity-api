2728950330b7a167966291c40aab9fdaf8c4f447
Victor Delgado <vdel26@gmail.com>
2015-11-16 15:15:18 -0800

    Devops version of the CAH API

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import http from 'http';
+import url from 'url';
+import { questions, answers } from './cards';
+
+const { floor: floor, random: random } = Math;
+const nquestions = questions.length, nanswers = answers.length;
+const question = () => { return questions[floor(random()*nquestions)] };
+const answer = () => { return answers[floor(random()*nanswers)] };
+const pick = () => { return { question: question(), answer: answer() } };
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, 'Content-Type: application/json');
+
+  let path = url.parse(req.url).pathname;
+  if (path === '/question')
+    res.write(question());
+  else if (path === '/answer')
+    res.write(answer());
+  else if (path === '/pick')
+    res.write(JSON.stringify(pick()));
+  else
+    res.write(`USAGE:
+               /question - get a random white card
+               /answer - get a random black card
+               /pick - get a question and answer randomly chosen`);
+
+  return res.end();
+
+});
+
+const port = parseInt(process.env.PORT, 10) || 5000;
+server.listen(port);
+console.log(`Listening at port ${port}`);
+
